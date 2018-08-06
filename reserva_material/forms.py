325d7813e03ee2797ddacb97bd80e64d98023f4a
Jorge Mauro <cadmauro98@gmail.com>
2018-08-06 13:09:30 -0300

    Formulário de empréstimo de material corrigido

--- reserva_material/forms.py
+++ reserva_material/forms.py
@@ -32,4 +32,4 @@ class emprestarForm(forms.Form):
     senha_empresta = forms.CharField(widget=forms.PasswordInput(attrs=attrs_senha_empresta))
     identidade_retira = forms.IntegerField(min_value=0, max_value=9999999999, initial="1111111111", widget=forms.NumberInput(attrs=attrs_idt_retira))
     senha_retira = forms.CharField(widget=forms.PasswordInput(attrs=attrs_senha_retira))
-    material_cautelado = forms.ModelMultipleChoiceField(queryset=Material.objects.filter(em_reserva=True), initial=lista_materiais, widget=forms.SelectMultiple(attrs=attrs_mat_cautelado))
+    material_cautelado = forms.ModelMultipleChoiceField(queryset=Material.objects.filter(em_reserva=True), widget=forms.SelectMultiple(attrs=attrs_mat_cautelado))
--- reserva_material/views/formularios.py
+++ reserva_material/views/formularios.py
@@ -2,10 +2,13 @@ from django.shortcuts import render
 from reserva_material.models import Pessoa, Quartel, Material, Cautela
 from reserva_material.forms import emprestarForm
 from django.contrib.auth.decorators import login_required
+from django.utils import timezone
+from django.http import HttpResponseRedirect, HttpResponse
+from django.urls import reverse
+import datetime
 
 @login_required
 def emprestar(request):
-    status = ''
     if request.method == 'POST':
         formulario = emprestarForm(request.POST)
         if formulario.is_valid():
@@ -13,23 +16,30 @@ def emprestar(request):
             senha_empresta = request.POST.get('senha_empresta')
             identidade_retira = request.POST.get('identidade_retira')
             senha_retira = request.POST.get('senha_retira')
-            material_cautelado = Material.objects.get(id=request.POST.get('material_cautelado'))
+            lista_materiais = request.POST.getlist('material_cautelado')
             quantitativo = 1
             try:
                 pessoa_empresta = Pessoa.objects.get(identidade_militar=identidade_empresta, senha=senha_empresta)
-            except:
-                status = 'Identidade ou senha da pessoa que empresta o material estão erradas!'
-            try:
                 pessoa_retira = Pessoa.objects.get(identidade_militar=identidade_retira, senha=senha_retira)
             except:
-                status = 'Identidade ou senha da pessoa que retira o material estão erradas!'
-            status = '%s - %s cautelado para %s %s com sucesso!' % (material_cautelado.nome_material, material_cautelado.numero_serie, pessoa_retira.posto_graduacao, pessoa_retira.nome_guerra)
-    formulario = emprestarForm()
-    context = {
-        'formulario': formulario,
-        'status': status,
-    }
-    return render(request, 'acoes/emprestar.html', context)
+                return HttpResponse('<center><h1>Usuários ou senhas <u>INCORRETAS</u>!</h1></center>', status=400)
+                # return HttpResponseRedirect(reverse('emprestar'))
+            for item in lista_materiais:
+                item = Material.objects.get(id=item)
+                item.em_reserva = False
+                item.em_cautela = True
+                cautela = Cautela(
+                pessoa_emprestou = pessoa_empresta,
+                pessoa_retirou = pessoa_retira,
+                material_cautelado = item,
+                quantitativo = quantitativo,
+                fim_cautela = timezone.now() + datetime.timedelta(days=30)
+                )
+                cautela.save()
+                item.save()
+    else:
+        formulario = emprestarForm()
+    return render(request, 'acoes/emprestar.html', {'formulario': formulario})
 
 @login_required
 def receber(request):
